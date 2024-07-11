c8361d1e0d8fb19c781e6cd0a1ce7622ddb083f6
ihab ahmed <ihab.ahmed@itworx.com>
2024-07-11 17:49:00 +0300

    update media storage keys

--- Dental/backend.py
+++ Dental/backend.py
@@ -2,12 +2,12 @@ from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
     account_name = 'dentall' # <storage_account_name>
-    account_key = 'oKkqC08HRXu0JIOlMTVQu1tfbBrOkYQq0mRuKA2H8c7LDmBF0nanwdJQLxDXGNBYv4pWxZK5bSRU+AStuFGeJg==' # <storage_account_key>
+    account_key = 'ORMbrA0VUK+pnHarjwL3yiDmK/C46h7g1D38HFECpqwKLycswd6fkBXSJ77i6/v0HB/67wCq6frz+AStWoulGw==' # <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
     account_name = 'dentall' # <storage_account_name>
-    account_key = 'oKkqC08HRXu0JIOlMTVQu1tfbBrOkYQq0mRuKA2H8c7LDmBF0nanwdJQLxDXGNBYv4pWxZK5bSRU+AStuFGeJg==' # <storage_account_key>
+    account_key = 'ORMbrA0VUK+pnHarjwL3yiDmK/C46h7g1D38HFECpqwKLycswd6fkBXSJ77i6/v0HB/67wCq6frz+AStWoulGw==' # <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
