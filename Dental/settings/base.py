140881fc4f8b32c51f73ddaa50c2c2e63817a72b
ihab ahmed <ihab.ahmed@itworx.com>
2024-07-11 16:34:23 +0300

    storage app adding

--- Dental/settings/base.py
+++ Dental/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'drf_spectacular',
     'rest_framework',
     'rest_framework.authtoken',
